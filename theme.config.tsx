40f54dfc8907d283c25b74850059d7ba787094a2
dejmedus <email96123@gmail.com>
2023-06-18 16:48:30 -0600

    restructure files

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import { useRouter } from "next/router";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>✨ notes ✨</span>,
+  logo: <span>✨ notes</span>,
   project: {
     link: "https://github.com/dejmedus/notes",
   },
   docsRepositoryBase: "https://github.com/dejmedus/notes",
   footer: {
     text: "✨ notes",
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – notes",
+      };
+    }
   },
 };
 
